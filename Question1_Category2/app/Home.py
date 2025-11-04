110ffe88ea99553d8fa5808dbceaeca7ae194e6d
hemtilva <hem.tilva@iitgn.ac.in>
2025-11-04 17:09:45 +0530

    cat2

--- Question1_Category2/app/Home.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="MLP Generators", page_icon="🤖", layout="wide")
-st.title("Welcome to MLP Generators")
-st.markdown("Use the sidebar to switch between pages.")
